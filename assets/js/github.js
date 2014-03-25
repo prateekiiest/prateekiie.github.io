0a6cc6fb56688d5677881897f79d1ef2f1181cac
Ryan Uber <ru@ryanuber.com>
2014-03-24 23:56:03 -0700

    Added some comments to the large javascript file that generates my GitHub activity feed.

--- assets/js/github.js
+++ assets/js/github.js
@@ -1,3 +1,5 @@
+// There is no way to get the short-hand month in Javascript, so instead
+// we call this wrapper function to wrap Date.
 function formatTime(timestamp) {
     var ts = new Date(timestamp);
     var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul',
@@ -5,20 +7,54 @@ function formatTime(timestamp) {
     return months[ts.getMonth()]+' '+ts.getDate();
 }
 
+// Represent a link to a repository
 function repoLink(name) {
     return '<a class="repo" href="https://github.com/'+name+'">'+name+'</a>';
 }
 
+// Represent a link to a specific branch of a repository
 function branchLink(repo, branch) {
     return '<a class="branch" '+
            'href="https://github.com/'+repo+'/tree/'+branch+'">'+
             branch+'</a>';
 }
 
+// Represent a link to an issue in a repository
 function issueLink(url, number) {
     return '<a class="pullreq" href="'+url+'">&#35;'+number+'</a>';
 }
 
+// Find all repositories for a user and fill in a blank div with the
+// contents after formatting it into style-able HTML.
+function githubRepos(username) {
+    var gh = new Octokit({username:username});
+    var user = gh.getUser(username);
+    var html = "";
+    user.getRepos().done(function(repos) {
+        repos.forEach(function(repo) {
+            html += "<div class='repo'>";
+            html += "<div class='name'>";
+            if (repo.fork) {
+                html += "<a href='"+repo.html_url+"'>"+repo.name+"</a></div>";
+                html += "<div class='contrib'>contributor</div>";
+                var html_url = "";
+                var real_repo = gh.getRepo(username, repo.name);
+                real_repo.getInfo().done(function(real_repo, html_url) {
+                    html_url = real_repo.parent.html_url;
+                });
+                alert(html_url);
+            }
+            html += "<div class='description'>"+repo.description+"</div>";
+            html += "</div>";
+        });
+        document.getElementById('github').style.display = 'none';
+        document.getElementById('github').innerHTML = html;
+        $('#github').fadeIn({duration: 800});
+    });
+}
+
+// Pull the GitHub API and fill in a blank div with the resulting content
+// after parsing it into HTML.
 function githubActivity(user) {
     var gh = new Octokit({username:user});
     var user = gh.getUser(user);
